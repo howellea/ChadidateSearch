2c151e0df887f07df9b707ecc50e5977458ba649
eddyhowell <howelleddy@gmail.com>
2025-04-12 19:21:58 -0500

    added data file to github

---
+++ src/data/candidates.ts
@@ -0,0 +1,45 @@
+
+import { Candidate } from '../interfaces/Candidate.interface';
+
+export const dummyCandidates: Candidate[] = [
+  {
+    name: "Erina Everhart",
+    username: "Ee1110",
+    location: "Tokyo, Japan",
+    email: "ee110@gmail.com",
+    company: "Microsoft",
+    bio: "JavaScript obsessed. Typical coffee lover. Hardcore travel buff.",
+    avatar: "https://randomuser.me/api/portraits/women/50.jpg",
+    html_url: "https://github.com/Ee1110",
+  },
+  {
+    name: "Alex Storkey",
+    username: "storkme",
+    location: "England",
+    email: "stork@not.gd",
+    company: "40AU",
+    bio: "Eat Sleep Code",
+    avatar: "https://randomuser.me/api/portraits/men/45.jpg",
+    html_url: "https://github.com/storkme",
+  },
+  {
+    name: "Sara Cortez",
+    username: "queSaraSara",
+    location: "Austin, TX",
+    email: "sarah.dev@gmail.com",
+    company: "sarahCodes",
+    bio: "DevOps Guru. i <3 film",
+    avatar: "https://randomuser.me/api/portraits/women/44.jpg",
+    html_url: "https://github.com/queSaraSara",
+  },
+  {
+    name: "Marcus Santos",
+    username: "marcus-santos-dev",
+    location: "Salvador, Bahia",
+    email: "marcus.santosnet@gmail.com",
+    company: "mr.coder",
+    bio: "Python all the way.",
+    avatar: "https://randomuser.me/api/portraits/men/22.jpg",
+    html_url: "https://github.com/marcus-santos-dev",
+  }
+];
